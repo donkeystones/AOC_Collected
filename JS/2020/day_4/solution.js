3a3dac1fb5cf24b750d860ebe72e24434fc55044
Oscar Bäck <oscar.back96@outlook.com>
2024-11-04 11:45:58 +0100

    day_4 solution.js part 1 trying to fix and start of part 2

--- JS/2020/day_4/solution.js
+++ JS/2020/day_4/solution.js
@@ -1,15 +1,28 @@
 let fs = require("fs");
 
+const fields_are_valid = (passport) => {
+console.log(passport.split(/\r\n+/g))
+}
+
 var data = fs.readFileSync('./input.txt','utf8');
 
-let data_arr = data.split("\n\n");
+console.log(data);
+
+let data_arr = data.split(/[\r\n]+/); //TODO: Fix so that it splits proper
+console.log(data_arr[0]);
+
 let valid_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-let sum = 0;
+let sum_part1 = 0;
+let sum_part2 = 0;
 for(let i = 0; i < data_arr.length; i++){
-	let pass_data = data_arr[i].split(/[\n\s]/);//Part 2?
-	if(valid_fields.every(item => data_arr[i].includes(item))) {
-		sum++;
+	if(valid_fields.every(item => data_arr[i].includes(item))) { //Part 1 check for every field that should be present
+		sum_part1++;
+		console.log(data_arr[i])
+		if(fields_are_valid(data_arr[i])){
+
+		}
 	}
 }
 
-console.log(sum);
+console.log(sum_part2);
+console.log(sum_part1);
