7e1f30111d9bd8fb175de409430437fce9ce86a6
Oscar Bäck <oscar.back96@outlook.com>
2024-11-05 10:34:50 +0100

    Part_2 almost done

--- JS/2020/day_4/solution.js
+++ JS/2020/day_4/solution.js
@@ -1,28 +1,85 @@
-let fs = require("fs");
 
-const fields_are_valid = (passport) => {
-console.log(passport.split(/\r\n+/g))
+let fs = require("fs");
+var data = fs.readFileSync('./input.txt','utf8');
+let data_arr = data.split("\r\n\r\n");
+let valid_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+let sum_part1 = 0, sum_part2 = 0;
+
+const validate_part_1 = (passport) => {
+	return valid_fields.every(item => passport.includes(item));
 }
 
-var data = fs.readFileSync('./input.txt','utf8');
+const validate_part_2 = (passport) => {
+	for(let i = 0; i < passport.length; i++){
+		let pass_field = passport[i];
+		let temp = pass_field.split(":");
+		if(temp.length > 1){
+			switch(temp[0]){
+				case "byr":
+					let year = parseInt(temp[1])
+					if(year > 2002 || year < 1920){
+						return false;
+					}
+					break;
+				case "iyr":
+					let year2 = parseInt(temp[1])
+					if(year2 > 2020 || year2 < 2010){
+						return false;
+					}
+					break;
+				case "eyr":
+					let year3 = parseInt(temp[1])
+					if(year3 > 2030 || year3 < 2020){
+						return false;
+					}
+					break;
+				case "hgt":
+					if(temp[1].includes("cm")){
+						let cm = parseInt(temp[1].replace(/^\D+/g, ''));
+						if(cm > 193 || cm < 150) {
+							return false;
+						}
+					}else{
+						let inches = parseInt(temp[1].replace(/^\D+/g, ''));
+						if(inches > 76 || inches < 59) {
+							return false;
+						}
+					}
+					break;
 
-console.log(data);
-
-let data_arr = data.split(/[\r\n]+/); //TODO: Fix so that it splits proper
-console.log(data_arr[0]);
-
-let valid_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-let sum_part1 = 0;
-let sum_part2 = 0;
-for(let i = 0; i < data_arr.length; i++){
-	if(valid_fields.every(item => data_arr[i].includes(item))) { //Part 1 check for every field that should be present
-		sum_part1++;
-		console.log(data_arr[i])
-		if(fields_are_valid(data_arr[i])){
-
+				case "hcl":
+					if(!temp[1].match(/#([0-9a-f]){6}$/gm)){
+						return false;
+					}
+					break;
+				case "ecl":
+					let valid_eye_color = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+					if(!valid_eye_color.includes(temp[1])) {
+						return false;
+					}
+					break;
+				case "pid":
+					if(!temp[1].match(/\d{9}/gm)) {
+						return false;
+					}
+					break;
+			}
 		}
+
+	}
+	return true;
+}
+
+for(let i = 0; i < data_arr.length; i++){
+	if(validate_part_1(data_arr[i])) {
+		sum_part1++;
+		let pass_data = data_arr[i].split(/[\n\s]/);//Part 2?
+		console.log(data_arr[i]);
+		console.log(pass_data);
+		if(validate_part_2(pass_data))
+			sum_part2++;
 	}
 }
 
-console.log(sum_part2);
 console.log(sum_part1);
+console.log(sum_part2);
