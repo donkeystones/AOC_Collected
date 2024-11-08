67d6d3d9e9b985fcd7d141ecc913828e86a28433
donkeystones <oscar.back96@outlook.com>
2024-11-08 13:35:24 +0100

    2020 day 6 part 2 done

--- JS/2020/day_6/solution.js
+++ JS/2020/day_6/solution.js
@@ -18,8 +18,20 @@ spaces indicate group change!
 
 */
 
-const parse_group_question_amount = (group) => {
-    return new Set([...group.replace(/\s/g,'')].join('')).size;
+const parse_group_question_amount = (groups) => {
+    return new Set([...groups.replace(/\s/g,'')].join('')).size;
+}
+
+const parse_group_question_amount_revised = (groups) => {
+    let group_arr = groups.split(/\r\n|\n/);
+    group_arr.sort((a,b) => a.length - b.length);
+    let smallest_group_answers = group_arr[0].split("");
+    for(let i = 1; i < group_arr.length; i++){
+        smallest_group_answers = smallest_group_answers.filter(
+            value => group_arr[i].split("").includes(value)
+        )
+    }
+    return smallest_group_answers.length;
 }
 
 const part1 = () => {
@@ -29,4 +41,12 @@ const part1 = () => {
     return sum;
 }
 
+const part2 = () => {
+    let sum = 0;
+    for(let group of data.split(/\r\n\r\n|\n\n/))
+        sum += parse_group_question_amount_revised(group);
+    return sum;
+}
+
 console.log(part1());
+console.log(part2());
