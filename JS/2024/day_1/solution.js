a41cc66323b1d572884b7dc24bc02ffac928faf0
donkeystones <oscar.back96@outlook.com>
2024-12-01 19:07:15 +0100

    2024 day 1 done

--- JS/2024/day_1/solution.js
+++ JS/2024/day_1/solution.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
 var data = fs.readFileSync('./input.txt','utf8');
 
-
+//Part 1
 let data_arr = data.split(/\r\n|\n/);
 
 let left_arr = [];
@@ -19,7 +19,16 @@ right_arr.sort();
 let sum = 0;
 
 for(let i = 0; i < left_arr.length; i++){
-    sum += right_arr[i] - left_arr[i]
+    let calc_diff = right_arr[i] - left_arr[i]
+    if(calc_diff < 0) calc_diff *= -1;
+
+    sum += calc_diff;
+}
+let sum_2 = 0;
+//part 2
+for(let i = 0; i < left_arr.length; i++){
+    sum_2 += left_arr[i] * right_arr.filter(x => x=== left_arr[i]).length;
 }
 
 console.log(sum);
+console.log(sum_2);
