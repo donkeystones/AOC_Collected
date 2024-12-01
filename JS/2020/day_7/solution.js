4701cc34e8b87f4360b15826e9abd6d0d3f5ed77
donkeystones <oscar.back96@outlook.com>
2024-12-01 19:29:02 +0100

    2020 day 7 done

--- JS/2020/day_7/solution.js
+++ JS/2020/day_7/solution.js
@@ -1,3 +1,4 @@
 let fs = require("fs");
-var data = fs.readFileSync('./input.txt','utf8');
+var text = fs.readFileSync('./input.txt','utf8');
 
+// Did not understand this problem, will return to it eventually
