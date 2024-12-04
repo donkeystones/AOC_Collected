6c4fc23c4541f936e9f478858efcfc8ac7c7d39a
Oscar Bäck <oscar.back96@outlook.com>
2024-12-04 16:24:10 +0100

    Update solution.js

--- JS/2020/day_8/solution.js
+++ JS/2020/day_8/solution.js
@@ -1,3 +1,27 @@
 let fs = require("fs");
 var data = fs.readFileSync('./input.txt','utf8');
 
+let instructions = data.split(/\r\n|\n/)
+
+let acc = 0;
+let history = [];
+
+for(let current_instruction_idx = 0; current_instruction_idx < instructions.length; ){
+    let current_instruction = instructions[current_instruction_idx].split(" ");
+    
+    if(history.length > 0 && history.findIndex((element) =>  element === current_instruction_idx) !== -1) break;
+    history.push(current_instruction_idx);
+
+    if(current_instruction[0] === "acc") {
+        acc += parseInt(current_instruction[1]); 
+        current_instruction_idx++;
+    }
+    
+    else if(current_instruction[0] === "nop") current_instruction_idx++;
+
+    else if(current_instruction[0] === "jmp") current_instruction_idx += parseInt(current_instruction[1]);
+
+    
+}
+
+console.log(acc);
